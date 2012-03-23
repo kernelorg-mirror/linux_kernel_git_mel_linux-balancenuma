b6f7da326248ee0d2b9501a506dafd928a1cbc16
Andrea Arcangeli <aarcange@redhat.com>
2012-03-23 20:56:34 +0100

    mm: numa: Structures for Migrate On Fault per NUMA migration rate limiting
    This defines the per-node data used by Migrate On Fault in order to
    rate limit the migration. The rate limiting is applied independently
    to each destination node.
    
    Signed-off-by: Andrea Arcangeli <aarcange@redhat.com>
    Signed-off-by: Mel Gorman <mgorman@suse.de>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -4449,6 +4449,11 @@ static void __paginginit free_area_init_core(struct pglist_data *pgdat,
 	int ret;
 
 	pgdat_resize_init(pgdat);
+#ifdef CONFIG_BALANCE_NUMA
+	spin_lock_init(&pgdat->balancenuma_migrate_lock);
+	pgdat->balancenuma_migrate_nr_pages = 0;
+	pgdat->balancenuma_migrate_next_window = jiffies;
+#endif
 	init_waitqueue_head(&pgdat->kswapd_wait);
 	init_waitqueue_head(&pgdat->pfmemalloc_wait);
 	pgdat_page_cgroup_init(pgdat);
